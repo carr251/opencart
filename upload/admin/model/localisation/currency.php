41a3bab91a613f2a4b576380079a3f136237a54a
carr251 <ymkzebeb@sharklasers.com>
2013-06-04 22:53:24 -0600

    Update currency.php

--- upload/admin/model/localisation/currency.php
+++ upload/admin/model/localisation/currency.php
@@ -1,4 +1,78 @@
 <?php
+
+// 	Bitcoin Helper 0.11
+
+/*
+	Bitcoin Helper - Copyright 2012, Jordan Hall
+	http://jordanhall.co.uk/projects/bitcoin-helper-php-bitcoin-class/
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*	Information about error codes
+
+	Certain functions within Bitcoin Helper (0.11 onwards) will return
+	numeric error codes in the case of unexpected error or failure.
+	
+	You should make sure to check the returned value for error codes
+	(intergers less than zero) in your code. The following reference 
+	shows the error numbers and an explanation.
+	
+	 * -1 = Network error retrieving data from bitcoincharts.com
+	 * -2 = Error decoding JSON data retrieved from bitcoincharts.com
+	 * -3 = Currency code not supported
+	 * -4 = Could not write to cache file - check permissions!
+*/
+
+// bitcoin_helper - A class consisting of various functions relating to Bitcoin
+class bitcoin_helper {
+	// convert_to_bitcoin - Converts a non-Bitcoin currency to Bitcoin and returns the Bitcoin value.
+	// As of bitcoin_helper 0.11
+	// currency_code = ISO 4217 formatted currency code, e.g. GBP, USD, etc.
+	// amount = Amount of currency to convert to Bitcoin
+	public function convert_to_btc($currency_code, $amount)
+	{	
+		// Get weighted prices. If an errors occurs, return the provided error code.
+		$weighted_prices = $this->get_weighted_prices();
+		if (is_numeric($weighted_prices) && $weighted_prices<0) return $weighted_prices;
+		
+		// Check if currency is supported
+		if (!array_key_exists($currency_code, $weighted_prices)) return -3;
+		if (!array_key_exists('24h', $weighted_prices[$currency_code])) return -3;
+		
+		// Perform necessary calculations and return Bitcoin amount
+		$btc_amount = 1 / $weighted_prices[$currency_code]['24h'];
+		return $btc_amount;
+	}
+	
+	// get_weighted_prices - Retrieves Bitcoin weighted prices JSON data from bitcoincharts.com and returns it as an array.
+	// Caching: This function will attempt to cache (to a file) the JSON data retrieved from bitcoincharts.com for up to one hour.
+	private function get_weighted_prices()
+	{
+		$cache_filename = "bitcoin_weighted_prices.json";
+		
+			$url = "http://bitcoincharts.com/t/weighted_prices.json";
+			$content = @file_get_contents($url);
+			if (!$content) return -1;
+	
+		$json = json_decode($content, true);
+		if (!$json) return -2;
+		return $json;
+	}
+
+}
+
 class ModelLocalisationCurrency extends Model {
 	public function addCurrency($data) {
 		$this->db->query("INSERT INTO " . DB_PREFIX . "currency SET title = '" . $this->db->escape($data['title']) . "', code = '" . $this->db->escape($data['code']) . "', symbol_left = '" . $this->db->escape($data['symbol_left']) . "', symbol_right = '" . $this->db->escape($data['symbol_right']) . "', decimal_place = '" . $this->db->escape($data['decimal_place']) . "', value = '" . $this->db->escape($data['value']) . "', status = '" . (int)$data['status'] . "', date_modified = NOW()");
@@ -11,8 +85,10 @@ class ModelLocalisationCurrency extends Model {
 	}
 	
 	public function editCurrency($currency_id, $data) {
-		$this->db->query("UPDATE " . DB_PREFIX . "currency SET title = '" . $this->db->escape($data['title']) . "', code = '" . $this->db->escape($data['code']) . "', symbol_left = '" . $this->db->escape($data['symbol_left']) . "', symbol_right = '" . $this->db->escape($data['symbol_right']) . "', decimal_place = '" . $this->db->escape($data['decimal_place']) . "', value = '" . $this->db->escape($data['value']) . "', status = '" . (int)$data['status'] . "', date_modified = NOW() WHERE currency_id = '" . (int)$currency_id . "'");
 
+			$this->db->query("UPDATE " . DB_PREFIX . "currency SET title = '" . $this->db->escape($data['title']) . "', code = '" . $this->db->escape($data['code']) . "', symbol_left = '" . $this->db->escape($data['symbol_left']) . "', symbol_right = '" . $this->db->escape($data['symbol_right']) . "', decimal_place = '" . $this->db->escape($data['decimal_place']) . "', value = '" . $this->db->escape($data['value']) . "', status = '" . (int)$data['status'] . "', date_modified = NOW() WHERE currency_id = '" . (int)$currency_id . "'");
+			
+		
 		$this->cache->delete('currency');
 	}
 	
@@ -23,6 +99,7 @@ class ModelLocalisationCurrency extends Model {
 	}
 
 	public function getCurrency($currency_id) {
+		
 		$query = $this->db->query("SELECT DISTINCT * FROM " . DB_PREFIX . "currency WHERE currency_id = '" . (int)$currency_id . "'");
 	
 		return $query->row;
@@ -35,6 +112,7 @@ class ModelLocalisationCurrency extends Model {
 	}
 		
 	public function getCurrencies($data = array()) {
+	$this->updateCurrencies();
 		if ($data) {
 			$sql = "SELECT * FROM " . DB_PREFIX . "currency";
 
@@ -101,15 +179,11 @@ class ModelLocalisationCurrency extends Model {
 		}
 	}	
 
-	public function updateCurrencies($force = false) {
+	public function updateCurrencies($force = true) {
 		if (extension_loaded('curl')) {
 			$data = array();
 			
-			if ($force) {
 				$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "currency WHERE code != '" . $this->db->escape($this->config->get('config_currency')) . "'");
-			} else {
-				$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "currency WHERE code != '" . $this->db->escape($this->config->get('config_currency')) . "' AND date_modified < '" .  $this->db->escape(date('Y-m-d H:i:s', strtotime('-1 day'))) . "'");
-			}
 			
 			foreach ($query->rows as $result) {
 				$data[] = $this->config->get('config_currency') . $result['code'] . '=X';
@@ -133,10 +207,14 @@ class ModelLocalisationCurrency extends Model {
 				if ((float)$value) {
 					$this->db->query("UPDATE " . DB_PREFIX . "currency SET value = '" . (float)$value . "', date_modified = '" .  $this->db->escape(date('Y-m-d H:i:s')) . "' WHERE code = '" . $this->db->escape($currency) . "'");
 				}
+				
 			}
 			
 			$this->db->query("UPDATE " . DB_PREFIX . "currency SET value = '1.00000', date_modified = '" .  $this->db->escape(date('Y-m-d H:i:s')) . "' WHERE code = '" . $this->db->escape($this->config->get('config_currency')) . "'");
-			
+			if  (($this->db->escape($currency)) == "BTC") {
+			$bh = new bitcoin_helper;
+			$this->db->query("UPDATE " . DB_PREFIX . "currency SET value = '" . $bh->convert_to_btc($this->db->escape($this->config->get('config_currency')), 1) . "', date_modified = '" .  $this->db->escape(date('Y-m-d H:i:s')) . "' WHERE code = '" . $this->db->escape($currency) . "'");
+		} 
 			$this->cache->delete('currency');
 		}
 	}
